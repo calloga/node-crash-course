13823eee8b67a0b299d503072f9e3987f2cedb12
Calloga <papeabdoucalloga98@gmail.com>
2022-10-03 01:57:23 +0000

    Routing and redirecting

--- server.js
+++ server.js
@@ -8,8 +8,31 @@ const server = http.createServer((req, res) => {
   // set headers content type
 
   res.setHeader("Content-Type", "text/html");
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+      break;
+
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+  console.log(path);
   // send html file
-  fs.readFile("./views/index.html", (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
