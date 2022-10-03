70fb7e4f17a7b26fb9733b47230f71448c205760
Calloga <papeabdoucalloga98@gmail.com>
2022-10-03 04:04:07 +0000

    Added the EJS package

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
 const express = require("express");
 
+const blogs = [
+  {
+    title: "Yoshi says their truth",
+    snippet:
+      "Lorem Ipsum is true and accurate enough for my purposes and for my purposes too",
+  },
+  {
+    title: "Mario says their truth",
+    snippet:
+      "Lorem Ipsum is true and accurate enough for my purposes and for my purposes too",
+  },
+  {
+    title: "Calloga says their truth",
+    snippet:
+      "Lorem Ipsum is true and accurate enough for my purposes and for my purposes too",
+  },
+];
 // express app
 
 const app = express();
 
+// register view engine
+
+app.set("view engine", "ejs");
 // listen for requests
 
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-// redirects
-
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "New Blog" });
 });
-
 // 404 page
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
