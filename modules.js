8f674cf2f5e990880e1e65a40e41664e4803ab72
Calloga <papeabdoucalloga98@gmail.com>
2022-10-03 00:34:55 +0000

    Exploring modules and require

---
+++ modules.js
@@ -0,0 +1,5 @@
+const { people, ages } = require("./people");
+
+const os = require("os");
+console.log(os.platform());
+console.log(os.homedir());
---
+++ people.js
@@ -0,0 +1,3 @@
+const people = ["yoshi", "ryu", "chun-li", "mario"];
+const ages = [20, 25, 30, 35];
+module.exports = { people, ages };
