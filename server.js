10fd9079aa61fb3fa03acfd9c0e42a7edd8a6157
Calloga <papeabdoucalloga98@gmail.com>
2022-10-03 01:35:24 +0000

    sending html file as a response to a request

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const http = require("http");
 
+const fs = require("fs");
+
 const server = http.createServer((req, res) => {
-  console.log("request made");
+  console.log(req.url, req.method);
+
+  // set headers content type
+
+  res.setHeader("Content-Type", "text/html");
+  // send html file
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
