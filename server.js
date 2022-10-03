a719adccf42a76612f5398609eef1512106ec03c
Calloga <papeabdoucalloga98@gmail.com>
2022-10-03 02:24:01 +0000

    Exploring NPM and installing nodemon and Lodash

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const http = require("http");
-
+const _ = require("lodash");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+  // lodash
 
+  const num = _.random(1, 20);
+  console.log(`num: ${num}`);
+
+  const greet = _.once(() => {
+    console.log("hello");
+  });
+
+  greet();
+  greet();
   // set headers content type
 
   res.setHeader("Content-Type", "text/html");
